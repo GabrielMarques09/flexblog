2959f7757284370a3017e268855ca7a919bbc9d7
Gabriel <gabrielmarrques@hotmail.com>
2020-04-16 14:59:48 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Curso realizado pela Origamid de Flexbox
+# Scroll suave em JavaScript puro
---
+++ js/script.js
@@ -0,0 +1,17 @@
+const nav = document.querySelectorAll('.menu-nav a[href^="#"]');
+
+nav.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+
+function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute('href')
+    const to = document.querySelector(id).offsetTop;
+    
+    window.scroll({
+        top: to,
+        behavior: "smooth"
+    });
+}
